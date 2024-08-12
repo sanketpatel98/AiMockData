a52b16e054d3797fb58a8868dddb36b9dab6d845
sanketpatel <patelsanketr98@gmail.com>
2024-08-12 13:53:12 -0400

    Revert "options are type restricted"
    This reverts commit 5963279dadf28374d4392c90cb50fbae77efc4b0.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import generateData from "./utils/dataGenerator.js";
 
-const aimockdata = async (number, parameters, topic) => {
-  return await generateData(number, parameters, topic);
+/**
+ * Main function to generate mock data using AI.
+ * 
+ * @param {number} number - The number of items to generate.
+ * @param {Array<{name: string, isArray: boolean}>} parameters - The parameters to use for generation.
+ * @param {string} topic - The topic of the data generation.
+ * @param {Object} [options] - Optional logging options.
+ * @param {boolean} [options.logPerformanceData] - Whether to log performance data.
+ * @param {boolean} [options.logSchema] - Whether to log the generated schema and prompt.
+ * @param {boolean} [options.logRawData] - Whether to log the raw response data.
+ * @returns {Promise<Array>} - The generated data.
+ */
+const aimockdata = async (number, parameters, topic, options = {}) => {
+  return await generateData(number, parameters, topic, options);
 };
 
 export default aimockdata;
